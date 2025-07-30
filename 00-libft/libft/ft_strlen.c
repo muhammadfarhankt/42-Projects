2c9ccf004d2b76bd2b55ec17cdd682dc0a0b7df1
Muhammad Farhan <farhanlatheefkt@gmail.com>
2025-07-30 23:59:06 +0400

    00-libft/ft_strlen.c

---
+++ 00-libft/libft/ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfarhan <mfarhan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 23:49:37 by mfarhan           #+#    #+#             */
+/*   Updated: 2025/07/30 23:58:35 by mfarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	char str[100];
+	printf("Enter a string : ");
+	scanf("%s", str);
+	int length = ft_strlen(str);
+	printf("Length of string : %d\n", length);
+	return 0;
+}
+*/
